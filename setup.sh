370991a1c9c90b1dbf627abe2585f5424021f993
Paul Philippov <paul@themactep.com>
2026-04-12 14:04:43 -0400

    Added binwalk, mtd-utils, and squashfs-tools to dependencies

--- setup.sh
+++ setup.sh
@@ -28,35 +28,35 @@ if [ -f /etc/os-release ]; then
         *debian*)
             sudo apt-get update
             sudo apt-get install -y \
-                autoconf build-essential bc bison ccache cpio cmake curl dialog file flex gawk git \
-                libncurses-dev libusb-1.0-0-dev make m4 nano perl python3 python3-pip python3-jsonschema \
-                rsync unzip u-boot-tools vim-tiny wget whiptail podman qemu-system-misc qemu-user-static
+                autoconf build-essential bc bison binwalk ccache cpio cmake curl dialog file flex gawk git \
+                libncurses-dev libusb-1.0-0-dev make m4 mtd-utils nano perl python3 python3-pip python3-jsonschema \
+                rsync squashfs-tools unzip u-boot-tools vim-tiny wget whiptail podman qemu-system-misc qemu-user-static
             ;;
         *)
             case "$ID" in
                 ubuntu|debian|linuxmint|zorin)
                     sudo apt-get update
                     sudo apt-get install -y \
-                        autoconf build-essential bc bison ccache cpio cmake curl dialog file flex gawk git \
-                        libncurses-dev libusb-1.0-0-dev make m4 nano perl python3 python3-pip python3-jsonschema \
-                        rsync unzip u-boot-tools vim-tiny wget whiptail podman qemu-system-misc qemu-user-static
+                        autoconf build-essential bc bison binwalk ccache cpio cmake curl dialog file flex gawk git \
+                        libncurses-dev libusb-1.0-0-dev make m4 mtd-utils nano perl python3 python3-pip python3-jsonschema \
+                        rsync squashfs-tools unzip u-boot-tools vim-tiny wget whiptail podman qemu-system-misc qemu-user-static
                     ;;
                 arch)
                     sudo pacman -Syu --needed --noconfirm \
-                        autoconf base-devel bc bison cpio cmake curl dialog file flex gawk git \
-                        m4 libnewt libusb make nano ncurses perl python python-pip rsync unzip \
+                        autoconf base-devel bc bison binwalk cpio cmake curl dialog file flex gawk git \
+                        m4 libnewt libusb make mtd-utils nano ncurses perl python python-pip rsync squashfs-tools unzip \
                         uboot-tools wget podman qemu-base qemu-user-static
                     ;;
                 fedora|rhel|centos)
                     sudo dnf install -y \
-                        autoconf gcc m4 make bc bison cpio cmake curl dialog file flex gawk git \
-                        nano ncurses-devel newt libusbx-devel perl rsync unzip uboot-tools wget \
+                        autoconf gcc m4 make bc bison binwalk cpio cmake curl dialog file flex gawk git \
+                        mtd-utils nano ncurses-devel newt libusbx-devel perl rsync squashfs-tools unzip uboot-tools wget \
                         podman qemu-system-x86 qemu-user-static
                     ;;
                 alpine)
                     sudo apk add \
-                        autoconf bash build-base bc bison cpio cmake curl dialog file findutils \
-                        flex gawk git grep m4 nano ncurses-dev newt libusb-dev make perl rsync \
+                        autoconf bash build-base bc bison binwalk cpio cmake curl dialog file findutils \
+                        flex gawk git grep m4 mtd-utils nano ncurses-dev newt libusb-dev make perl rsync squashfs-tools \
                         unzip uboot-tools wget podman qemu-system-x86_64 qemu-user-static
                     ;;
                 *)
